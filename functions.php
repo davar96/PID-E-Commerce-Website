f57585303dc04cbd1a26f0adabb873483e47d198
David Arustamyan <david.arustamyan.96@gmail.com>
2022-02-06 23:01:37 +0100

    Fetched products and data for specialPrice section

--- functions.php
+++ functions.php
@@ -2,6 +2,15 @@
     // Require MySql Connection
     require('database/DBController.php');
 
+    // Require Product class
+    require('database/Product.php');
+
     // DBController Object
     $db = new DBController();
+
+    // Product object
+    $product = new Product($db);
+
+    // print_r($product->getData());
+
 ?>
