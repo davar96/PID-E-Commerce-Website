9229596bca627e5b1c6bc9191c828aa7f5748df0
David Arustamyan <david.arustamyan.96@gmail.com>
2022-03-18 20:57:33 +0100

    polishing

--- Template/_banner-ads.php
+++ Template/_banner-ads.php
@@ -2,8 +2,8 @@
  
         <section id="banner-adds">
             <div class="container py-5 text-center">
-                <img src="assets/audi_rs6.jpg" alt="banner1" class="img-fluid">
-                <img src="assets/audi_rs6.jpg" alt="banner1" class="img-fluid">
+                <img src="assets/add1.png" alt="ad1" class="img-fluid">
+                <img src="assets/add2.png" alt="ad2" class="img-fluid">
             </div>
         </section>
 
